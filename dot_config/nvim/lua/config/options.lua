e0b4840e26f37c2266c81604a6eb584a21426473
sorvon <sorvon@qq.com>
2025-08-06 19:19:11 +0800

    灵活使用osc52与win32yank

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,30 +1,37 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- vim.g.clipboard = {
---   name = 'wslclipboard',
---   copy = {
---     ["+"] = { "nvim-clip", "set" },
---     ["*"] = { "nvim-clip", "set" }
---   },
---   paste = {
---     ["+"] = { "nvim-clip", "get" },
---     ["*"] = { "nvim-clip", "get" }
---   },
--- }
--- vim.g.clipboard = {
---   name = 'osc52',
---   copy = {
---     ["+"] = require('vim.ui.clipboard.osc52').copy('+'),
---     ["*"] = require('vim.ui.clipboard.osc52').copy('+'),
---   },
---   paste = {
---     ["+"] = { "nvim-clip", "get" },
---     ["*"] = { "nvim-clip", "get" }
---   },
--- }
+vim.g.clipboard = {
+  name = "osc52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("+"),
+  },
+  paste = {
+    ["+"] = function()
+      local t = require("vim.ui.clipboard.osc52").paste("+")()
+      if type(t) == "table" then
+        for key, value in pairs(t) do
+          t[key] = string.gsub(value, "\r", "")
+        end
+      end
+      return t
+    end,
+    ["*"] = function()
+      local t = require("vim.ui.clipboard.osc52").paste("+")()
+      if type(t) == "table" then
+        for key, value in pairs(t) do
+          t[key] = string.gsub(value, "\r", "")
+        end
+      end
+      return t
+    end,
+  },
+}
 
-vim.g.clipboard = "win32yank"
+if vim.env.ZELLIJ ~= nil then
+  vim.g.clipboard = "win32yank"
+end
 vim.g.mkdp_open_to_the_world = 1
 vim.g.autoformat = false
 vim.opt.fileencodings = "ucs-bom,utf-8,gbk,default,latin1"
